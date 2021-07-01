954dc9f89dae114fcf31b9f27ceab2c1bd74fae6
eiji.okuda <eiji.okuda@matera.com>
2021-06-30 22:35:41 -0300

    Fix teste

--- src/main/java/chapter06/a_try_catch_finally_first/SectionStore.java
+++ src/main/java/chapter06/a_try_catch_finally_first/SectionStore.java
@@ -1,11 +1,16 @@
 package chapter06.a_try_catch_finally_first;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SectionStore {
     public List<RecordedGrip> retrieveSection(String sectionName) {
-        // dummy return until we have a real implementation
-        return new ArrayList<>();
+        try {
+            FileInputStream stream = new FileInputStream(sectionName);
+        } catch (Exception e) {
+            throw new StorageException("retrieval error", e);
+        }
+        return new ArrayList<RecordedGrip>();
     }
 }
--- src/main/java/chapter06/a_try_catch_finally_first/StorageException.java
+++ src/main/java/chapter06/a_try_catch_finally_first/StorageException.java
@@ -1,4 +1,7 @@
 package chapter06.a_try_catch_finally_first;
 
 public class StorageException extends RuntimeException{
+    public StorageException(String message, Exception e) {
+
+    }
 }
